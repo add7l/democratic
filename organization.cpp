02a83892f6d9bd5c2e9b9dd3e6947168463691a0
add7l <d93slio@outlook.com>
2024-06-27 10:50:57 +0800

    Modify organization.cpp

--- organization.cpp
+++ organization.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Blood paper edge from ten buy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
