1d01c5c6dd5bbffebf7f4b989089e8c2fcc874b6
add7l <d93slio@outlook.com>
2024-08-04 12:43:09 +0800

    Modify than.cpp

--- than.cpp
+++ than.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# East laugh others always everything very.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
