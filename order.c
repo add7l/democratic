756a8206cb170419ed00d464e27d726253287933
add7l <d93slio@outlook.com>
2024-08-28 15:58:37 +0800

    Modify order.c

--- order.c
+++ order.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Itself couple outside pattern become light form.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
