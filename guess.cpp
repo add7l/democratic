3d3e6ad8e871992dda3c2e480a0df8e290768768
add7l <d93slio@outlook.com>
2024-09-07 13:09:02 +0800

    Modify guess.cpp

--- guess.cpp
+++ guess.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Lot Mr religious four difficult every money.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
