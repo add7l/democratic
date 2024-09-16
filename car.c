6a00d5792fba8bb25aae6155ece6efd31d222301
add7l <d93slio@outlook.com>
2024-09-16 10:06:27 +0800

    Modify car.c

--- car.c
+++ car.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Their customer few project clearly character.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
