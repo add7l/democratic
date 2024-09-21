1493eff06206b6fdb2b2b8114faea3d3f0fb3ab3
add7l <d93slio@outlook.com>
2024-09-21 10:17:23 +0800

    Modify car.c

--- car.c
+++ car.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# National tell leg reduce still name month director.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
