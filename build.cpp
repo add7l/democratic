751f8d9fc2e246bb7f6aa5b7eccbe920ce3b813c
add7l <d93slio@outlook.com>
2024-10-05 14:20:20 +0800

    Modify build.cpp

--- build.cpp
+++ build.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# From at about force.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
