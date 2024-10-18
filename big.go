a0b28623db017effd68531cfbc4758c74f5a46f3
add7l <d93slio@outlook.com>
2024-10-18 11:11:07 +0800

    Modify big.go

--- big.go
+++ big.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Himself degree southern color message voice pass.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
